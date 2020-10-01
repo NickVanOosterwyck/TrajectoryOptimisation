d0d155f43cde166245bd59f13f1d2f530afdffbe
NickVanOosterwyck <36815611+NickVanOosterwyck@users.noreply.github.com>
2020-10-01 16:43:25 +0200

    update nedschroef for-back

--- examples/Ex2_Ned_ForBack.m
+++ examples/Ex2_Ned_ForBack.m
@@ -3,7 +3,7 @@
 clear; clc; close all;
 addpath(genpath([fileparts(matlab.desktop.editor.getActiveFilename),'\..']))
 
-%% Forward
+%% cheb7 forward
 clear input
 % required
 input.sMechanism = 'Nedschroef';
@@ -21,24 +21,58 @@ input.d_Tl = 5;
 input.isTimeResc = true;
 input.isPosResc = true;
 
-cheb7 = TrajOpt(input);
-cheb7.optimizeTrajectory();
+cheb7For = TrajOpt(input);
+cheb7For.optimizeTrajectory();
 
-%% Pause1
+%% pause1
 clear input
 % required
 input.sMechanism = 'Nedschroef';
 input.sTrajType = 'pause';
 input.timeA = 0.07375;
-input.timeB = 0.1;
+input.timeB = 0.1675;
 input.posA = 3.0299;
 input.posB = 3.0299;
 
 pause1 = TrajOpt(input);
 pause1.optimizeTrajectory();
 
+%% cheb7 backwards
+clear input
+% required
+input.sMechanism = 'Nedschroef';
+input.sTrajType = 'cheb';
+input.timeA = 0.1675;
+input.timeB = 0.2250;
+input.posA = 3.0299;
+input.posB = 0;
+input.DOF = 2;
+input.sSolver = 'quasi-newton';
+
+% optional
+input.d_J = 4;
+input.d_Tl = 5;
+input.isTimeResc = true;
+input.isPosResc = true;
+
+cheb7Bac = TrajOpt(input);
+cheb7Bac.optimizeTrajectory();
+
+%% pause2
+clear input
+% required
+input.sMechanism = 'Nedschroef';
+input.sTrajType = 'pause';
+input.timeA = 0.2250;
+input.timeB = 0.3;
+input.posA = 0;
+input.posB = 0;
+
+pause2 = TrajOpt(input);
+pause2.optimizeTrajectory()
+
 %% combine
-total  = [cheb7 pause1];
+total  = [cheb7For pause1 cheb7Bac pause2];
 
 %% plot
 fig = TrajPlot(input);
